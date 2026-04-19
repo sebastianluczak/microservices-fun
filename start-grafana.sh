cd32b73d8adab52a5ec7eeb1d89c77b42863602a
Sebastian <sebastian.m.luczak@gmail.com>
2026-04-19 17:41:12 +0200

    Works with monitoring (#1)

--- start-grafana.sh
+++ start-grafana.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cp -r $INTERNAL_GRAFANA_DEVENV_DIR/share/grafana/public/ grafana/public
+
 grafana server --homepath ${DEVENV_ROOT}/grafana
--- user-tracker/src/app.module.ts
+++ user-tracker/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
